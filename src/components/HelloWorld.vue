a515c10f98ede884c54b1f42014c800bd022a598
“Akaisorani” <1224067801@qq.com>
2022-02-25 15:10:11 +0800

    upd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <img class="logo" src="@/assets/logo.jpg"/>
+    <div class="main">
+      <div class="result">
+        <div class="result-number">
+          <div class="result-number-item" v-for="(item, index) in result" :key="index">{{ item }}</div>
+        </div>
+        <div class="btn shadow" v-if="!working" @click="startRandom">抽 奖</div>
+        <div class="btn shadow" v-else @click="stopRandom">停</div>
+      </div>
+      <div class="gender">
+        <img v-if="male" src="@/assets/male.png"/>
+        <img v-else src="@/assets/female.png"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+let task = null;
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      working: false,
+      male: true,
+      result: '000'
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    startRandom() {
+      this.$data.working = true
+      task = setInterval(() => {
+        this.generateLottery()
+      }, 20)
+    },
+    stopRandom() {
+      this.$data.working = false
+      clearInterval(task)
+    },
+    formatNumber(n) {
+      let result = n.toString()
+      let length = result.length
+      for (let i = length; i < 3; i++) {
+        result = '0' + result
+      }
+      return result
+    },
+    generateLottery() {
+      this.$data.result = this.formatNumber(Math.floor(Math.random() * 100 + 1))
+      this.$data.male = Math.random() > 0.5
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.logo {
+  position: absolute;
+  width: 25vw;
+  right: 0;
+  bottom: 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.main {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  left: 5vw;
+  top: 20vh;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.result {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-a {
-  color: #42b983;
+
+.result-number {
+  display: flex;
+  flex-direction: row;
+}
+
+.result-number-item {
+  font-size: 15vw;
+  margin: 0 2vw;
+  padding: 0 2vw;
+  border: #2c3e50 10px solid;
+  border-radius: 10px;
+  color: #7924cb;
+  height: 35vh;
+  line-height: 35vh;
+  background-color: white;
+  font-weight: bold;
+}
+
+.gender {
+  margin-left: 3vw;
+}
+
+.gender img {
+  height: 25vh;
+  margin-top: 5vh;
+}
+
+.btn {
+  text-align: center;
+  margin-top: 8vh;
+  font-size: 3vw;
+  width: 15vw;
+  height: 6vw;
+  line-height: 6vw;
+  color: rgb(203, 32, 32);
+  background: linear-gradient(rgb(251, 220, 15), rgb(255, 166, 3));
+  border-radius: 1vw;
+  font-weight: bold;
+}
+
+.shadow {
+  position: relative;
+  max-width: 20vw;
+  box-shadow: 0px 1px 4px rgba(255, 166, 3, 0.3),
+  0px 0px 20px rgba(255, 166, 3, 0.1) inset;
+}
+
+.shadow::before,
+.shadow::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+}
+
+.shadow::before,
+.shadow::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  bottom: 15px;
+  left: 10px;
+  width: 50%;
+  height: 20%;
+}
+
+.shadow::before,
+.shadow::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  bottom: 15px;
+  left: 10px;
+  width: 50%;
+  height: 20%;
+  box-shadow: 0 15px 10px rgba(0, 0, 0, 0.7);
+  transform: rotate(-3deg);
+}
+
+.shadow::after {
+  right: 10px;
+  left: auto;
+  transform: rotate(3deg);
 }
 </style>
